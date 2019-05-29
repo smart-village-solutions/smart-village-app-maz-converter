558a89dc6a9a489cd2310e3702f9bff41de1edac
Sebastian Peter <sebastian.peter@ikusei.de>
2019-05-29 16:50:16 +0200

    Add Empty Exporter Class with TODO List

---
+++ app/services/exporter.rb
@@ -0,0 +1,8 @@
+class Exporter
+
+  # Steps for Exporter
+  # - collect all parsed JSON Data and send it to server
+  # - mark all parsed json records who were sent to the server
+  # - save response from server and log
+  # - send notifications
+end

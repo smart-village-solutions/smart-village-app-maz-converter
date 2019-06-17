c65325934473824074944ba4085a63c35ace48c6
Sebastian Peter <sebastian.peter@ikusei.de>
2019-06-17 15:51:25 +0200

    Update authentication process to new "server to server" version
    * copied new oauth code from xml converter
    * necessary to implement server-to-server authenticstion process

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  post 'api', to: "api#create"
-  match "oauth/confirm_access", to: "oauth#confirm_access", via: [:get, :post]
+  post "api", to: "api#create"
+  match "oauth/confirm_access", to: "oauth#confirm_access", via: %i[get post]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

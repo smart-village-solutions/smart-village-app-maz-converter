6ca5f523116e2e58f13873feac38869582b129c2
Alexander Kahl <ak@sodosopa.io>
2019-06-17 17:15:29 +0200

    Rename route /api -> /json to match upstream

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post 'api', to: "api#create"
+  post 'json', to: "api#create"
   match "oauth/confirm_access", to: "oauth#confirm_access", via: [:get, :post]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

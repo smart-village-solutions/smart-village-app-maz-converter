4035e7914c81cae9c93e8771781d03863a36232b
Sebastian Peter <sebastian.peter@ikusei.de>
2019-06-18 19:59:35 +0200

    Complete create method for Api
    * Maz send its news as single news in json format and  the json is not wrapped
    * because of this this commit prepares incoming params to be saved in json_data
    *  if save is successful api create method will send back a status code created and a success message
    * if not an error response is created
    * add spec to spec this process

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,22 @@
+# frozen_string_literal: true
+
 class ApiController < ApplicationController
   def create
-    Record.create(json_data: params[data])
+    params.delete(:action)
+    params.delete(:controller)
+    maz_json = params.as_json
+
+    @record = Record.new(json_data: maz_json)
+
+    if @record.save
+      byebug
+      render json: {
+        message: "News Article was successfully imported"
+      }, status: 201
+    else
+      render json: {
+        error_message: "Error. News Item couldn't be imported. Please contact the admin."
+      }, status: 400
+    end
   end
 end

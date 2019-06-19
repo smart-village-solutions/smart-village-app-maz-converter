a9ed678a19c96561c5e54290e52147953a98543b
Sebastian Peter <sebastian.peter@ikusei.de>
2019-06-19 12:39:30 +0200

    Add before action to Api Controller to restrict access
    * /json endpoint need access restriction somthat only maz/rnd and localhost can acces the endpoint
    * commit will implement this feature by adding a before action to the api controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApiController < ApplicationController
+  before_action :restrict_access
+
   def create
     params.delete(:action)
     params.delete(:controller)
@@ -18,4 +20,16 @@ class ApiController < ApplicationController
       }, status: 400
     end
   end
+
+  private
+
+    def restrict_access
+      whitelist = ["0.0.0.0", "127.0.0.1", "18.194.118.243"].freeze
+
+      unless whitelist.include? request.remote_ip
+        render json: {
+          error_message: "Access denied"
+        }, status: 401
+      end
+    end
 end

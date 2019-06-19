107879d5b4941b86c641135bdf964bf20eab437f
Sebastian Peter <sebastian.peter@ikusei.de>
2019-06-19 18:40:12 +0200

    Implement changes to make importing news possible
    * wrap news_data in an array because graphql converter expects it as an array and will not convert if it is not an array
    * only parse geo location for address if geo_location values are present in maz news data
    *  remove setting of audited_comment, because when  callback tries to set it the application throws an unknown attribute error
    * parse url will only parse the first url od portal_urls Array. An additional guard clause was necessary
    * fix bug for news_type value
    * remove empty set_full_version method

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,70 +1,72 @@
 # frozen_string_literal: true
 
 class Record < ApplicationRecord
-  audited only: :updated_at
+  audited
   before_save :convert_maz_json_to_sva_json
   after_save :send_sva_json_to_server
 
   private
 
-  def convert_maz_json_to_sva_json
-    news_data = parse_single_news_from_json(self.json_data)
-    self.sva_json_data = { news_item: news_data }
-  end
+    def convert_maz_json_to_sva_json
+      news_data = parse_single_news_from_json(json_data)
+      self.sva_json_data = { news: [news_data] }
+    end
 
-  def parse_single_news_from_json(json_hash)
-    {
-      external_id: json_hash.dig("_id"),
-      title: json_hash.dig("title", "value"),
-      author: "Tim Test",
-      publication_date: json_hash.dig("publication_date", "value"),
-      published_at: json_hash.dig("publish_date", "value"),
-      show_publish_date: json_hash.dig("show_publish_date", "value"),
-      news_type: json_hash.dig("show_publish_date", "value"),
-      address: parse_address(json_hash),
-      source_url: parse_url(json_hash),
-      content_blocks: ContentBlockParser.perform(json_hash)
-    }
-  end
-
-  def set_full_version
-    ""
-  end
-
-  def parse_address(json_hash)
-    {
-      city: json_hash.dig("location_name", "value"),
-      geo_location: {
-        latitude: json_hash.dig("geo_location", "value", "latitude"),
-        longitude: json_hash.dig("geo_location", "value", "longitude")
-      }
-    }
-  end
-
-  def parse_url(json_hash)
-    # TODO: prüfen ob mit http wenn mit dann description absolute
-    # otherwise relative
-    # prüfen ob url mit http://www.maz-online anfängt und nur falls ja parsen.
-    return nil if json_hash.dig("portal_urls").blank?
-    json_hash.dig("portal_urls").map do |url|
+    def parse_single_news_from_json(json_hash)
       {
-        url: url["portal_url"],
+        external_id: json_hash.dig("_id"),
+        title: json_hash.dig("title", "value"),
+        author: "Tim Test",
+        publication_date: json_hash.dig("publication_date", "value"),
+        published_at: json_hash.dig("publish_date", "value"),
+        show_publish_date: json_hash.dig("show_publish_date", "value"),
+        news_type: "news article",
+        address: parse_address(json_hash),
+        source_url: parse_url(json_hash),
+        content_blocks: ContentBlockParser.perform(json_hash)
+      }
+    end
+
+    def parse_address(json_hash)
+      if json_hash.dig("geo_location", "value", "latitude").blank? || json_hash.dig("geo_location", "value", "longitude").blank?
+        {
+          city: json_hash.dig("location_name", "value")
+        }
+      else
+        {
+          city: json_hash.dig("location_name", "value"),
+          geo_location: {
+            latitude: json_hash.dig("geo_location", "value", "latitude"),
+            longitude: json_hash.dig("geo_location", "value", "longitude")
+          }
+        }
+      end
+    end
+
+    def parse_url(json_hash)
+      # TODO: prüfen ob mit http wenn mit dann description absolute
+      # otherwise relative
+      # prüfen ob url mit http://www.maz-online anfängt und nur falls ja parsen.
+      return nil if json_hash.dig("portal_urls").blank?
+      return nil if json_hash.dig("portal_urls").first.blank?
+
+      {
+        url: json_hash.dig("portal_urls").first["portal_url"],
         description: ""
       }
     end
-  end
 
-  def send_sva_json_to_server
-    access_token = Authentication.new.access_token
-    url = Rails.application.credentials.target_server[:url]
+    def send_sva_json_to_server
+      access_token = Authentication.new.access_token
+      url = Rails.application.credentials.target_server[:url]
 
-    begin
-      result = ApiRequestService.new(url, nil, nil, self.sva_json_data, {Authorization: "Bearer #{access_token}"}).post_request
-      self.update_columns(updated_at: Time.now, audit_comment: result.body)
-    rescue => e
-      self.update_columns(updated_at: Time.now, audit_comment: e)
+      begin
+        result = ApiRequestService.new(url, nil, nil, sva_json_data, Authorization: "Bearer #{access_token}").post_request
+        update_columns(updated_at: Time.now)
+      rescue StandardError => e
+        update_columns(updated_at: Time.now)
+      end
     end
-  end
 
 end
 

db84ecccabc3eaa3e570d7f7a46f47c367ae2824
Sebastian Peter <sebastian.peter@ikusei.de>
2019-06-21 12:46:32 +0200

    Remove unnecessary exporter class

--- app/services/exporter.rb
+++
@@ -1,8 +0,0 @@
-class Exporter
-
-  # Steps for Exporter
-  # - collect all parsed JSON Data and send it to server
-  # - mark all parsed json records who were sent to the server
-  # - save response from server and log
-  # - send notifications
-end

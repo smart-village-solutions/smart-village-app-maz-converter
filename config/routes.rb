261df02f8aa33ce5468f6c2c6a770d25a373200b
Sebastian Peter <sebastian.peter@ikusei.de>
2019-07-12 11:55:37 +0200

    Add necessary delete route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+
   post "json", to: "api#create"
+  delete "/json/:id", to: "api#destroy", defaults: { format: "json" }
+
   match "oauth/confirm_access", to: "oauth#confirm_access", via: %i[get post]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
